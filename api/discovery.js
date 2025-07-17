9d2ef4d57373ee348663063dea5a0b4394900842
James Stout <james.stout@gmail.com>
2025-07-17 14:44:17 -0400

    Update discovery.js

--- api/discovery.js
+++ api/discovery.js
@@ -1,5 +1,5 @@
 // api/discovery.js
-const handler = async (req, res) => {
+module.exports = function handler(req, res) {
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
